a6ab83557bd366e9c3407b20f9645fc387c744f8
Robert Kramer <robert.n.kramer@gmail.com>
2019-07-22 14:02:36 -0700

    changed App to class component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, {Component} from "react";
 import Navbar from "./components/Navbar";
 import MainContent from "./components/MainContent";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <MainContent />
-    </div>
-  );
+class App extends Component {
+  state = {
+    tweets: []
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <MainContent />
+      </div>
+    );
+  }
 }
 
 export default App;
