dbc371f1d96534a5055601775c5b3e8bab4f56d1
Robert Kramer <robert.n.kramer@gmail.com>
2019-07-26 11:35:30 -0700

    added axios

--- client/src/components/Tweets.js
+++ client/src/components/Tweets.js
@@ -10,19 +10,32 @@ class Tweets extends Component {
   }
 
   componentDidMount() {
-    fetch("/api/tweets")
-      .then(res => res.json())
-      .then(tweets => this.setState({ tweets }));
+    const USER_TOKEN =
+      "AAAAAAAAAAAAAAAAAAAAAM5O%2FQAAAAAA4PUqURKvlaYZE123WLpGFsLFIuo%3D8Zee34fRfseVOhCH53BbhlcpPd4jzmXL9Xy34o2dBDngAfmkfu";
+    const URL =
+      "https://api.twitter.com/1.1/search/tweets.json?q=nasa&result_type=popular&count=1";
+
+    const AuthStr = "Bearer ".concat(USER_TOKEN);
+    axios
+      .get(URL, { headers: { Authorization: AuthStr } })
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log("error " + error);
+      });
   }
 
   render() {
     return (
-      <div className='tweet-list'>
+      <div className="tweet-list">
         <h1>Tweets</h1>
         <ul>
-          {this.state.tweets.map(tweet => 
-            <li className='tweet' key={tweet.id}>{tweet.tweet}</li>
-          )}
+          {this.state.tweets.map(tweet => (
+            <li className="tweet" key={tweet.id}>
+              {tweet.tweet}
+            </li>
+          ))}
         </ul>
       </div>
     );
