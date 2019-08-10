fd5d533eb43bf29437c77bee70a949c3064d7bbf
Robert Kramer <robert.n.kramer@gmail.com>
2019-08-10 16:30:46 -0700

    moved variables

--- api/getToken.js
+++ api/getToken.js
@@ -1,23 +1,23 @@
 const axios = require("axios");
 
+const KEY = process.env.TWITTER_API_KEY;
+const SECRET = process.env.TWITTER_API_SECRET;
+const ENCODED_KEY = new Buffer.from(KEY + ":" + SECRET).toString("base64");
+
+const AuthStr = `Basic ${ENCODED_KEY}`;
+const URL = "https://api.twitter.com/oauth2/token";
+
+const config = {
+  headers: {
+    Authorization: AuthStr,
+    "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+    Accept: "application/json"
+  }
+};
+
+const data = "grant_type=client_credentials"
+
 const getToken = () => {
-  const KEY = process.env.TWITTER_API_KEY;
-  const SECRET = process.env.TWITTER_API_SECRET;
-  const ENCODED_KEY = new Buffer.from(KEY + ":" + SECRET).toString("base64");
-
-  const AuthStr = `Basic ${ENCODED_KEY}`;
-  const URL = "https://api.twitter.com/oauth2/token";
-
-  const config = {
-    headers: {
-      Authorization: AuthStr,
-      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-      Accept: "application/json"
-    }
-  };
-
-  const data = "grant_type=client_credentials"
-
   return axios
     .post(URL, data, config)
     .then(response => {
