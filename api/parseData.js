adc6b7b4574102165d5010788a7e1ebc32335160
Robert Kramer <robert.n.kramer@gmail.com>
2019-08-11 11:29:57 -0700

    refactored data.map

--- api/parseData.js
+++ api/parseData.js
@@ -1,10 +1,9 @@
 const parseData = data => {
-  const tweets = [];
-  data.map(tweet => {
+  return data.map(tweet => {
     const { id, created_at, text, retweet_count, favorite_count } = tweet;
     const { name, screen_name, profile_image_url_https } = tweet.user;
 
-    tweets.push({
+    return {
       id,
       created_at,
       name,
@@ -13,10 +12,8 @@ const parseData = data => {
       text,
       retweet_count,
       favorite_count
-    });
+    };
   });
-
-  return tweets;
 };
 
 module.exports = parseData;
