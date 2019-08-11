5991d6d1efb861e8b6726b239031cb38733222d1
Robert Kramer <robert.n.kramer@gmail.com>
2019-08-11 13:47:10 -0700

    refactored authorization

--- api/getToken.js
+++ api/getToken.js
@@ -2,22 +2,22 @@ const axios = require("axios");
 
 const KEY = process.env.TWITTER_API_KEY;
 const SECRET = process.env.TWITTER_API_SECRET;
-const ENCODED_KEY = new Buffer.from(KEY + ":" + SECRET).toString("base64");
-
-const AuthStr = `Basic ${ENCODED_KEY}`;
 const URL = "https://api.twitter.com/oauth2/token";
 
-const data = "grant_type=client_credentials"
+const data = "grant_type=client_credentials";
 const config = {
+  auth: {
+    username: KEY,
+    password: SECRET
+  },
   headers: {
-    Authorization: AuthStr,
     "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
     Accept: "application/json"
   }
 };
 
 const getToken = async () => {
-  const res = await axios
+  return await axios
     .post(URL, data, config)
     .then(response => {
       return response.data.access_token;
@@ -25,7 +25,6 @@ const getToken = async () => {
     .catch(error => {
       console.log("error: " + error);
     });
-  return res;
 };
 
 module.exports = getToken;
